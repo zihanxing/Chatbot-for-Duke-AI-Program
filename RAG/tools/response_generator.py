e276bb4369b7bfd742f3fdf48bae6c6b484f6643
Yibo Liu <lyb979428261@gmail.com>
2024-04-18 10:44:52 -0400

    Update UI

--- RAG/tools/response_generator.py
+++ RAG/tools/response_generator.py
@@ -1,17 +1,44 @@
-from RAG.tools.retriever import semantic_search
+from RAG.tools.retriever import semantic_search, model
 
-import cohere
+import requests
+import numpy as np
+
+API_URL = "https://ufooozidbpadrm6t.us-east-1.aws.endpoints.huggingface.cloud"
+
+headers = {
+    "Accept": "application/json",
+    "Authorization": "Bearer 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",
+    "Content-Type": "application/json"
+}
 
 
 def generate_with_rag(query: str):
     context = semantic_search(query, 1)
     print(context)
-    prompt = f'query: {query}\n\ncontext: {context}'
-    return generate_response(prompt), context
+    if cosine_sim_comparison(context, query) >= 0.5:
+        prompt = f'query: {query}\n\ncontext: {context}'
+        return generate_response(prompt), context
+    else:
+        return generate_response(query)
 
 
 def generate_response(query):
-    co = cohere.Client('YJsYd0iwJjjg222OpO0XzvHQT8zRviVUKBfdgN6c')
-    response = co.generate(prompt=query, model='command-light')
-    print(response.generations[0].text)
-    return response.generations[0].text
+    response = requests.post(API_URL, headers=headers, json={'inputs': query, 'parameters': {}})
+    print(response.json())
+    return response.json()[0]['generated_text']
+
+
+def cosine_sim_comparison(context: str, query: str):
+    context_embedding = model.encode(context)
+    query_embedding = model.encode(query)
+
+    return cosine_sim_calculation(context_embedding, query_embedding)
+
+
+def cosine_sim_calculation(vec1, vec2):
+    dot_prod = np.dot(vec1, vec2)
+
+    norm_vec1 = np.linalg.norm(vec1)
+    norm_vec2 = np.linalg.norm(vec2)
+
+    return dot_prod / (norm_vec1 * norm_vec2)
--- RAG/tools/retriever.py
+++ RAG/tools/retriever.py
@@ -2,6 +2,8 @@ from RAG.tools.vectorizer import class_name, connect_client
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
+model = SentenceTransformer('sentence-transformers/all-roberta-large-v1')
+
 
 def semantic_search(query: str, chunk_num: int) -> str:
     client = connect_client()
@@ -10,7 +12,6 @@ def semantic_search(query: str, chunk_num: int) -> str:
         response = collection.query.fetch_objects(include_vector=True)
         vectors = [o.vector['default'] for o in response.objects]
 
-        model = SentenceTransformer('sentence-transformers/all-roberta-large-v1')
         query_vector = model.encode(query)
 
         dot_product = np.dot(vectors, query_vector)
